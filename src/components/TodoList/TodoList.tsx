5eb17fa2c36c54ef301a7519c540a369f020ac96
yoonyounghoon <yunyounghoon23@gmail.com>
2021-07-26 16:20:16 +0900

    버튼별 전체,완료,미완료 목록 보여주기

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -16,20 +16,41 @@ export type Todo = {
 };
 
 function TodoList({ todos, onToggle, onRemove, onRemoveCompleted }: TodoListProps) {
+  // 완료 목록 보기, 미완료 목록보기 , 전체 보기
+  // done을 기준으로 체킹
+  const [status, setStatus] = useState('All');
+
+  const handleStatus = (status: string) => {
+    setStatus(status);
+  };
+
   return (
     <div className="TodoList">
       <div>
-        {todos.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove} />
-        ))}
+        {status === 'all' &&
+          todos.map((todo) => <TodoListItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove} />)}
+        {status === 'active' &&
+          todos.map(
+            (todo) => !todo.done && <TodoListItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove} />,
+          )}
+        {status === 'completed' &&
+          todos.map(
+            (todo) => todo.done && <TodoListItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove} />,
+          )}
       </div>
       {todos.length > 0 && (
         <div className="footer">
           <p className="footer-left">{todos.filter((todo) => !todo.done).length} item left</p>
           <div className="footer-right">
-            <button className="footer-btn all">All</button>
-            <button className="footer-btn active">Active</button>
-            <button className="footer-btn completed">Completed</button>
+            <button className="footer-btn all" onClick={() => handleStatus('all')}>
+              All
+            </button>
+            <button className="footer-btn active" onClick={() => handleStatus('active')}>
+              Active
+            </button>
+            <button className="footer-btn completed" onClick={() => handleStatus('completed')}>
+              Completed
+            </button>
           </div>
           <button onClick={onRemoveCompleted}>Clear completed</button>
         </div>
